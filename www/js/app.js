66ee3e4346815f205e018db43e0e710c2a0d1d08
jloures <john_loures@hotmail.com>
2016-09-17 18:23:47 -0400

    Adding games almost works perfectly

--- www/js/app.js
+++ www/js/app.js
@@ -1,24 +1,61 @@
-// Ionic Starter App
+angular.module('ionicApp', ['ionic'])
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+.controller('AppCtrl', function($scope, $ionicPopup, $timeout, $ionicModal) {
+  
+  $scope.games = [];
+  $scope.numberOfgames = 0;
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-
-      // Don't remove this line unless you know what you are doing. It stops the viewport
-      // from snapping when text inputs are focused. Ionic handles this internally for
-      // a much nicer keyboard experience.
-      cordova.plugins.Keyboard.disableScroll(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
+  $ionicModal.fromTemplateUrl('templates/modal.html', {
+    scope: $scope
+  }).then(function(modal) {
+    $scope.modal = modal;
   });
-})
+  
+  $scope.createGame = function(game) {
+    console.log(game)
+    var newGame = Object.assign({}, game);
+    //do error checking here
+    if( game.gameName.trim() === "" ) {
+      $ionicPopup.alert({
+        title: 'Error',
+        template: 'Game Name has to be valid!'
+      });
+      return;
+    }
+
+    if( game.teamA.trim() === "") {
+      $ionicPopup.alert({
+        title: 'Error',
+        template: 'Team A name has to be valid!'
+      });
+      return;
+    }
+
+    if( game.teamB.trim() === "") {
+      $ionicPopup.alert({
+        title: 'Error',
+        template: 'Team B name has to be valid!'
+      });
+      return;
+    }
+
+    if( game.teamA === game.teamB ) {
+      $ionicPopup.alert({
+        title: 'Error',
+        template: 'Two teams cannot have the same name!'
+      });
+      return;
+    }
+
+    newGame.gameId = $scope.numberOfgames++;
+    $scope.games.push(newGame);
+    $scope.modal.hide();
+    //set all properties to default
+    game.gameName = "";
+    game.teamA = "";
+    game.teamB = "";
+    game.hasBODCount = false;
+    game.hasSuperOptimizer = false;
+    game.hasBODCount = false;
+  }
+});
